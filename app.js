b0b8473678130405431fc48499ce5963e36939a7
Nawaz <nzan576@gmail.com>
2024-04-09 03:07:31 +0530

    Project Setup

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import courseRoutes from "./routes/courseRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import enrollmentRoutes from "./routes/enrollmentRoutes.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/enrollment", enrollmentRoutes);
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
